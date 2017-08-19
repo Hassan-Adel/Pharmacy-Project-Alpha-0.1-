d1a8eaf6fc5021e21db4c72a5c3bb3ec55622d1d
Hassan-Adel <hasanadel555@hotmail.com>
2017-08-19 17:28:15 +0200

    Adding Migrations

---
+++ PharmacyProject.Web/Migrations/20170817221031_Initial Migration3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PharmacyProject.Migrations
+{
+    public partial class InitialMigration3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
