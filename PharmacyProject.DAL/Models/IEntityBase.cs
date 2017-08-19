9e9ad8438fe04f9853febe92233c07f4bc1a9171
Hassan-Adel <hasanadel555@hotmail.com>
2017-08-19 17:23:49 +0200

    Adding Models

---
+++ PharmacyProject.DAL/Models/IEntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyProject.DAL.Models
+{
+    public interface IEntityBase
+    {
+        int Id { get; set; }
+    }
+}
---
+++ PharmacyProject.DAL/Models/Language.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PharmacyProject.DAL.Models
+{
+    public class Language : IEntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(7)]
+        public string Locale { get; set; }
+        [MaxLength(100)]
+        public string Description { get; set; }
+        public ICollection<ContentText> ContentTexts { get; set; }
+    }
+}
