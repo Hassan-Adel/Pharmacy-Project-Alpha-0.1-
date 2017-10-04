82bbf92a26e613eacec56ed392c5654870e128b2
Hassan-Adel <hasanadel555@hotmail.com>
2017-10-04 13:26:26 +0200

    Adding Services Abstracs

---
+++ PharmacyProject.Web/Server/Services/Abstract/IEmailSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyProject.Server.Services.Abstract
+{
+    public interface IEmailSender
+    {
+        Task<bool> SendEmailAsync(MailType type, EmailModel emailModel, string extraData);
+        bool SendEmail(EmailModel emailModel);
+    }
+}
---
+++ PharmacyProject.Web/Server/Services/Abstract/ISmsSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyProject.Server.Services.Abstract
+{
+    public interface ISmsSender
+    {
+        Task<bool> SendSmsTwillioAsync(string number, string message);
+        Task<bool> SendSmsFastSmsAsync(string number, string message);
+    }
+}
