815629fc8b0a714cf25db6f6434396895326e2c3
Hassan-Adel <hasanadel555@hotmail.com>
2017-10-04 13:26:59 +0200

    Adding Account ViewModels

---
+++ PharmacyProject.Web/Server/ViewModels/AccountViewModels/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyProject.Server.ViewModels.AccountViewModels
+{
+    public class Class
+    {
+    }
+}
---
+++ PharmacyProject.Web/Server/ViewModels/Enums.cs
@@ -0,0 +1,13 @@
+﻿namespace PharmacyProject.Server.ViewModels
+{
+    public enum ExternalLoginStatus
+    {
+        Ok = 0,
+        Error = 1,
+        Invalid = 2,
+        TwoFactor = 3,
+        Lockout = 4,
+        CreateAccount = 5
+
+    }
+}
